74b693cdc3aedc3d8d8397301766a40cc3f402ae
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-11 23:17:32 -0400

    Intro directory

---
+++ JavaBeginner/Intro/src/com/jethro/Main.java
@@ -0,0 +1,13 @@
+package com.jethro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        int age = 35;
+//        int temperature = 20;
+          int myAge = 30;
+          int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
