af8465f928c9ef0eca4b38e33fed23dae6c283c4
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-12 12:50:10 -0400

    working with primitive type declarations

---
+++ JavaBeginner/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+package com.jethro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      byte age = 30;
+      long viewsCount = 3_123_456_789L;
+      float price = 10.99F;
+      char letter = 'A';
+      boolean isEligible = true;
+    }
+}
