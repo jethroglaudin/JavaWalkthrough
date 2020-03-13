5c26887fc339573d8bddb98ec83f854f06d867bf
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-12 22:44:31 -0400

    updated Main.java

--- JavaBeginner/Intro/src/com/jethro/Main.java
+++ JavaBeginner/Intro/src/com/jethro/Main.java
@@ -1,13 +1,15 @@
 package com.jethro;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        int age = 35;
-//        int temperature = 20;
-          int myAge = 30;
-          int herAge = myAge;
-        System.out.println(herAge);
+      byte age = 30;
+      Date now = new Date();
+      System.out.println(now);
+
+
     }
 }
