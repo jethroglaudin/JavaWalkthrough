79052f6101691f7b735641473d8573632ad316f4
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-14 16:29:20 -0400

    updated FormattingNumbers.java

--- JavaBeginner/Types/FormattingNumbers.java
+++ JavaBeginner/Types/FormattingNumbers.java
@@ -5,13 +5,13 @@ import java.text.NumberFormat;
 public class Main {
 
     public static void main(String[] args) {
-//        NumberFormat currency = NumberFormat.getCurrencyInstance(); // Factory method creates a new object
-//          NumberFormat percent = NumberFormat.getPercentInstance();
+        // NumberFormat currency = NumberFormat.getCurrencyInstance(); // Factory method
+        // creates a new object
+        // NumberFormat percent = NumberFormat.getPercentInstance();
 
-
-//        String result = percent.format(123456.891);
-//        String result = percent.format(0.1);
-          String result2 = NumberFormat.getPercentInstance().format(0.1);
+        // String result = percent.format(123456.891);
+        // String result = percent.format(0.1);
+        String result2 = NumberFormat.getPercentInstance().format(0.1);
 
         System.out.println(result2);
     }
