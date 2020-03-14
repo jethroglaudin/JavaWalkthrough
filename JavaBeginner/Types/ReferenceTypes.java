8e2e1d50274366c6396761ea393fcca012632e6e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-13 20:47:18 -0400

    Reference Types

---
+++ JavaBeginner/Types/ReferenceTypes.java
@@ -0,0 +1,15 @@
+package com.jethro;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      byte age = 30;
+      Date now = new Date();
+      System.out.println(now);
+
+
+    }
+}
