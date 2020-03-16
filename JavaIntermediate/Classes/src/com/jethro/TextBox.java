f89c1ed88a67d7d8502cf26e0e85b670538cddc2
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-15 23:31:33 -0400

    created TextBox class that has one field and two methods

---
+++ JavaIntermediate/Classes/src/com/jethro/TextBox.java
@@ -0,0 +1,13 @@
+package com.jethro;
+
+public class TextBox {
+    public String text; // Field
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        this.text = "";
+    }
+}
