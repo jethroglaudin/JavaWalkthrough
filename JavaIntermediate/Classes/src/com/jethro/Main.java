4729a924bef63554834da1e0a01fa7cf0a22523a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-16 22:21:17 -0400

    Procedural  Programming

--- JavaIntermediate/Classes/src/com/jethro/Main.java
+++ JavaIntermediate/Classes/src/com/jethro/Main.java
@@ -4,12 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        var textBox1 = new TextBox(); // Create new instance of the class
-        textBox1.setText("rBox 1");
-        System.out.println(textBox1.text.toUpperCase());
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourlyRate = 20;
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
+        System.out.println(wage);
+    }
+
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + (extraHours * hourlyRate);
     }
 }
