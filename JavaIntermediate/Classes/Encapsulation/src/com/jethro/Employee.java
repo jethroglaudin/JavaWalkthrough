d9d3187e48033e65babf5c5c219e1b92af13bb7f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-16 22:46:59 -0400

    Working with Getters and Setters

--- JavaIntermediate/Classes/Encapsulation/src/com/jethro/Employee.java
+++ JavaIntermediate/Classes/Encapsulation/src/com/jethro/Employee.java
@@ -1,10 +1,31 @@
 package com.jethro;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or less");
+        this.hourlyRate = hourlyRate;
+    }
+
 }
--- JavaIntermediate/Classes/Encapsulation/src/com/jethro/Main.java
+++ JavaIntermediate/Classes/Encapsulation/src/com/jethro/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(50_000);
+//        employee.getBaseSalary();
+        employee.setHourlyRate(20);
+//        employee.getHourlyRate();
         int wage = employee.calculateWage(10);
 
         System.out.println(wage);
