9703c6e38f00398849542efcd8cd75a7558a5325
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-18 23:09:00 -0400

    Created Console class

---
+++ JavaIntermediate/MortgageCalculator/src/com/jethro/Console.java
@@ -0,0 +1,18 @@
+package com.jethro;
+
+import java.util.Scanner;
+
+public class Console {
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " " + max);
+        }
+        return value;
+    }
+}
