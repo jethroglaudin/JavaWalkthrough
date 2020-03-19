109be410335d95baf9a2dca074cbac68e0ca9a97
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-18 23:13:33 -0400

    overloaded the readNumber method

--- JavaIntermediate/MortgageCalculator/src/com/jethro/Console.java
+++ JavaIntermediate/MortgageCalculator/src/com/jethro/Console.java
@@ -3,12 +3,16 @@ package com.jethro;
 import java.util.Scanner;
 
 public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+    public static double readNumber(String prompt){
+        return scanner.nextDouble();
+
+    }
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.print(prompt);
-            value = scanner.nextFloat();
+            value = scanner.nextDouble();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter a value between " + min + " " + max);
--- JavaIntermediate/MortgageCalculator/src/com/jethro/Main.java
+++ JavaIntermediate/MortgageCalculator/src/com/jethro/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("************MORTGAGE CALCULATOR************");
         System.out.println("Calculate your monthly Mortgage Payments");
 
+        
         final int PRINCIPAL = (int) Console.readNumber("Principal ($1K - $1M): ", 1_000, 1_000_000);
         final float ANNUAL_INTEREST_RATE = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
         final byte ANNUAL_PERIODS = (byte) Console.readNumber("Period (Years)", 1, 30);
