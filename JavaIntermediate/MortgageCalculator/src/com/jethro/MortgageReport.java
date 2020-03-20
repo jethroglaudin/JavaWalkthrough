e6de6ed70d46ae4ac27d3c5e9aa2fdd8c3db7c47
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-19 23:06:05 -0400

    Refactored the usage of the NumberFormat abstract base class

--- JavaIntermediate/MortgageCalculator/src/com/jethro/MortgageReport.java
+++ JavaIntermediate/MortgageCalculator/src/com/jethro/MortgageReport.java
@@ -4,19 +4,15 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
-
-    public void paymentsLeft() {
-        for (double balance : calculator.getRemainingBalances())
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-    }
-
     public void printPaymentSchedule() {
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(calculator.calculateMortgage());
+        String mortgageFormatted = currency.format(calculator.calculateMortgage());
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
@@ -24,4 +20,9 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("------------------");
     }
+    public void paymentsLeft() {
+        for (double balance : calculator.getRemainingBalances())
+            System.out.println(currency.format(balance));
+    }
+
 }
