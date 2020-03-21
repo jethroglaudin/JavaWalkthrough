e5e21ef0ac6160a7f74ac917cec3c238d0d8d277
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-20 23:02:11 -0400

    Instantiated a new UIControl object

--- JavaIntermediate/Inheritance/src/com/jethro/Main.java
+++ JavaIntermediate/Inheritance/src/com/jethro/Main.java
@@ -3,6 +3,8 @@ package com.jethro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var control = new UIControl();
+	    control.disable();
+        System.out.println(control.isEnabled());
     }
 }
