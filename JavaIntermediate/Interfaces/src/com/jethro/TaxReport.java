dd6533ff005f12aa38dc98084698fbc205d8b6d2
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-22 17:02:11 -0400

    created Tax Report class

--- JavaIntermediate/Interfaces/src/com/jethro/TaxReport.java
+++ JavaIntermediate/Interfaces/src/com/jethro/TaxReport.java
@@ -1,4 +1,14 @@
 package com.jethro;
 
 public class TaxReport {
+    private TaxCalculator calculator;
+
+    public TaxReport() {
+        calculator = new TaxCalculator(100_000);
+    }
+
+    public void show() {
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
 }
