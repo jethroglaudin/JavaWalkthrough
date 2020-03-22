3a17f2af5cb09c00c226a2983c53026c62a1986a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-21 23:48:35 -0400

    Created UIControl array and instatiated a new TextBox and new CheckBox object. Itterated through each element call each objects render method

--- JavaIntermediate/Inheritance/src/com/jethro/Main.java
+++ JavaIntermediate/Inheritance/src/com/jethro/Main.java
@@ -3,30 +3,34 @@ package com.jethro;
 public class Main {
 
     public static void main(String[] args) {
+
+        UIControl[] controls = { new TextBox(), new CheckBox() };
+        for (var control : controls){
+            control.render();
+        }
+    }
 //        var control = new UIControl(true);
 //        var textBox = new TextBox();
 //        show(textBox); // Upcasting Cast the object to it's parent
 
-        var point1 = new Point(1, 2);
-        var point2 = new Point(1, 2);
-        System.out.println(point1.equals(point2));
-        System.out.println(point1.equals(new TextBox()));
-        System.out.println(point1.hashCode());
-        System.out.println(point2.hashCode());
+//        var point1 = new Point(1, 2);
+//        var point2 = new Point(1, 2);
+//        System.out.println(point1.equals(point2));
+//        System.out.println(point1.equals(new TextBox()));
+//        System.out.println(point1.hashCode());
+//        System.out.println(point2.hashCode());
 
 
-
-    }
     // Upcasting
-    public static void show (UIControl control) {
-        if (control instanceof  TextBox) {
-            // will return true if object is an instance of the textBox
-            // DownCasting
-            var textBox = (TextBox)control;
-            textBox.setText("Hello World");
-        }
-
-
-        System.out.println(control);
-    }
+//    public static void show (UIControl control) {
+//        if (control instanceof  TextBox) {
+//            // will return true if object is an instance of the textBox
+//            // DownCasting
+//            var textBox = (TextBox)control;
+//            textBox.setText("Hello World");
+//        }
+//
+//
+//        System.out.println(control);
+//    }
 }
