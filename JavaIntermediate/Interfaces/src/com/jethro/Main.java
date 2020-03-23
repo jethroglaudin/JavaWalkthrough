fb6407f435f82d11ca82e47228abd7138851180f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-22 20:24:07 -0400

    injected the new TaxCalculator2021 class into setCalculator

--- JavaIntermediate/Interfaces/src/com/jethro/Main.java
+++ JavaIntermediate/Interfaces/src/com/jethro/Main.java
@@ -6,6 +6,10 @@ public class Main {
 	// write your code here
         var calculator = new TaxCalculator2020(100_000);
         var report = new TaxReport(calculator);
+        report.show();
+
+        report.setCalculator(new TaxCalculator2021()); // setter injection
+        report.show();
 
     }
 }
