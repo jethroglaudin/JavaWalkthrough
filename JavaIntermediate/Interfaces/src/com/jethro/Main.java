0f250d2e87773786148feab51629cb95c2e83092
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-22 20:32:25 -0400

    Method Injection

--- JavaIntermediate/Interfaces/src/com/jethro/Main.java
+++ JavaIntermediate/Interfaces/src/com/jethro/Main.java
@@ -5,11 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         var calculator = new TaxCalculator2020(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
+        var report = new TaxReport();
+        report.show(calculator);
+        report.show(new TaxCalculator2021());
 
-        report.setCalculator(new TaxCalculator2021()); // setter injection
-        report.show();
+//        var report = new TaxReport(calculator);
+//        report.show();
+//
+//        report.setCalculator(new TaxCalculator2021()); // setter injection
+//        report.show();
 
     }
 }
--- JavaIntermediate/Interfaces/src/com/jethro/TaxReport.java
+++ JavaIntermediate/Interfaces/src/com/jethro/TaxReport.java
@@ -4,15 +4,15 @@ public class TaxReport {
 
     private TaxCalculator calculator;
 
-    public TaxReport(TaxCalculator calculator) { // constructor injection.
-        this.calculator = calculator;
-    }
+//    public TaxReport(TaxCalculator calculator) { // constructor injection.
+//        this.calculator = calculator;
+//    }
 
-    public void show() {
+    public void show(TaxCalculator calculator) { // method injection
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
-    public void setCalculator(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
+//    public void setCalculator(TaxCalculator calculator) {
+//        this.calculator = calculator;
+//    }
 }
