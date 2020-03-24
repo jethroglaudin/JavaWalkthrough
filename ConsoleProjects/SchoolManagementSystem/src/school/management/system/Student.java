423dbe6b25763121f4c28b4cdafc325a8a978f8d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-24 17:28:11 -0400

    updated Student class

--- ConsoleProjects/SchoolManagementSystem/src/school/management/system/Student.java
+++ ConsoleProjects/SchoolManagementSystem/src/school/management/system/Student.java
@@ -50,4 +50,44 @@ public class Student {
     public void updateFeesPaid(int fees) {
         feesPaid+=fees;
     }
+
+    /**
+     *
+     * @return id of the student
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @return name of the student
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @return the grade of the student
+     */
+    public int getGrade() {
+        return grade;
+    }
+
+    /**
+     *
+     * @return fees paid by the student.
+     */
+    public int getFeesPaid() {
+        return feesPaid;
+    }
+
+    /**
+     *
+     * @return the total fees of the student
+     */
+    public int getFeesTotal() {
+        return feesTotal;
+    }
 }
