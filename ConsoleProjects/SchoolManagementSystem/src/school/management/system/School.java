61809baf20cdfed06a87f7e039a47e82aa34e318
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-24 17:27:09 -0400

    Created School class

---
+++ ConsoleProjects/SchoolManagementSystem/src/school/management/system/School.java
@@ -0,0 +1,59 @@
+package school.management.system;
+
+import java.util.List;
+
+/**
+ * Many teachers, many students
+ * Implements teachers and students using an ArrayList
+ */
+public class School {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+    /**
+     * new school object is created.
+     * @param teachers list of teachers in the school.
+     * @param students list of students in the school.
+     */
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void setTotalMoneyEarned(int totalMoneyEarned) {
+        this.totalMoneyEarned = totalMoneyEarned;
+    }
+
+    public void setTotalMoneySpent(int totalMoneySpent) {
+        this.totalMoneySpent = totalMoneySpent;
+    }
+}
