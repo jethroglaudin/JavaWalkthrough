eaaab9dfab9631db7f93f3e160072ca9f065a37a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-23 22:53:33 -0400

    School Mangement System project

---
+++ ConsoleProjects/SchoolManagementSystem/src/school/management/system/Main.java
@@ -0,0 +1,10 @@
+package school.management.system;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello World");
+    }
+}
