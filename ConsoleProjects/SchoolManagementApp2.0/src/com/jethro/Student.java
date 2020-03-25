74a57b73272b688442eda75dc29684071ac3b9a8
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-25 18:26:48 -0400

    added Student class

---
+++ ConsoleProjects/SchoolManagementApp2.0/src/com/jethro/Student.java
@@ -0,0 +1,41 @@
+package com.jethro;
+
+public class Student {
+    private int id;
+    private String name;
+    private short grade;
+    private double feesPaid;
+    private double feesTotal;
+
+    public Student(int id, String name, short grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public void payFees(int fees) {
+        feesPaid += fees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public short getGrade() {
+        return grade;
+    }
+
+    public double getFeesPaid() {
+        return feesPaid;
+    }
+
+    public double getFeesTotal() {
+        return feesTotal - feesPaid;
+    }
+
+
+}
