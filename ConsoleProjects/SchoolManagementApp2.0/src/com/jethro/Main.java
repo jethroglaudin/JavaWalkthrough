c42c69dfe222d23e212ed7de8465d564db79449a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-25 17:17:23 -0400

    Created School Management App 2.0 which is a refactored version of the original School Management App

---
+++ ConsoleProjects/SchoolManagementApp2.0/src/com/jethro/Main.java
@@ -0,0 +1,20 @@
+package com.jethro;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("----Welcome to U-Manage----");
+        System.out.println("Student Information System");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            var input = scanner.next().toLowerCase();
+            if(input.equals("yes")){
+                break;
+            }
+        }
+    }
+}
