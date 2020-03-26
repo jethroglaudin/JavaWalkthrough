ec2809293fb3a0548d304d04b1ecd58f9675e86d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-26 17:01:02 -0400

    modified Teacher class

--- ConsoleProjects/SchoolManagementApp2.0/src/com/jethro/Teacher.java
+++ ConsoleProjects/SchoolManagementApp2.0/src/com/jethro/Teacher.java
@@ -1,4 +1,76 @@
 package com.jethro;
 
+import java.text.NumberFormat;
+
+/**
+ * This class is responsible for keeping track of
+ * teacher's id, name, salary
+ */
 public class Teacher {
+
+    private int id;
+    private String name;
+    private double salary;
+    private double salaryEarned;
+
+    /**
+     * Creates a new Teacher object
+     *
+     * @param id     id for teacher
+     * @param name   name of the teacher
+     * @param salary salary of the teacher
+     */
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.salaryEarned = 0;
+    }
+
+    /**
+     * @return the id for the teacher
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return name of the teacher
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the salary of the teacher.
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+
+    /**
+     * Adds to salary
+     *
+     * @param salary
+     */
+    public void receiveSalary(int salary) {
+        salaryEarned = salary;
+//        School.updateTotalMoneySpent(salary);
+    }
+
+
+    /**
+     * Update the salary
+     *
+     * @param salary
+     */
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher: " + name + " Total salary earned so far is  " + NumberFormat.getCurrencyInstance().format(salaryEarned);
+    }
 }
