d99a112fcc0f5c518bb1c53cb2f46e01b7fcd8b0
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-29 22:47:53 -0400

    created two methods wihtin the ExceptionsDemo class to go through how exceptions work through java.lang

--- JavaAdvanced/Advanced/src/com/jethro/Main.java
+++ JavaAdvanced/Advanced/src/com/jethro/Main.java
@@ -1,9 +1,11 @@
 package com.jethro;
 
+import com.jethro.exceptions.ExceptionsDemo;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        ExceptionsDemo.show();
     }
 }
--- JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
@@ -1,4 +1,11 @@
 package com.jethro.exceptions;
 
 public class ExceptionsDemo {
+    public static void show() {
+        sayHello(null);
+    }
+
+    public static void sayHello(String name) {
+        System.out.println(name.toUpperCase());
+    }
 }
