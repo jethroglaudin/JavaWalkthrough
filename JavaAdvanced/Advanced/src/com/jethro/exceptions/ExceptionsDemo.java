c06b6c44ad1c5141128a66ccbc2e0dfa19c5424d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-29 23:08:28 -0400

    Working with catching expressions with try catch blocks

--- JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
@@ -1,11 +1,23 @@
 package com.jethro.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
+        try{
+            var reader = new FileReader("file.txt");
+            System.out.println("File opened");
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 
     public static void sayHello(String name) {
         System.out.println(name.toUpperCase());
     }
+
+//    var reader = new FileReader("file.txt"); // example of checked exception Java compiler telling us to handle. Checked at compile time;
+
 }
