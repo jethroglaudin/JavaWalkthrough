0d6ec92959de00db8f7226fb103f590c1b800a5e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-02 23:26:11 -0400

    Workign with try -with-resources statements for writing less code and alterntative for using finally block. Where the java compilier will add a final block explicitly

--- JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
@@ -2,30 +2,33 @@ package com.jethro.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try{
-            reader = new FileReader("file.txt");
+//        FileReader reader = null;
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...")
+        ) { // try with resources statement
             var value = reader.read();
             new SimpleDateFormat().parse("");
             System.out.println("File opened");
         } catch (IOException  | ParseException e) { // catching multiple exceptions
             System.out.println("Could not read data.");
         }
-        finally { // we can release extra resources like file handlers & DB connections/ finally block always gets executed
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+//        finally { // we can release extra resources like file handlers & DB connections/ finally block always gets executed
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
     }
 
     public static void sayHello(String name) {
