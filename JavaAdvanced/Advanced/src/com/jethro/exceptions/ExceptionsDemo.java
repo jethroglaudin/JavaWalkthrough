30aea41ec75cb5c92af5c7bdcbf6bcfaedbcbcd0
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-02 23:09:41 -0400

    Working with catching multiple exceptions

--- JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
@@ -2,14 +2,19 @@ package com.jethro.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
         try{
             var reader = new FileReader("file.txt");
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
             System.out.println("File opened");
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
+        } catch (IOException  | ParseException e) { // catching multiple exceptions
+            System.out.println("Could not read data.");
         }
 
     }
