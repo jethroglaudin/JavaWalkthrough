4c5be3695237326563154d8f6efd87bdfe1e246c
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-03 22:35:23 -0400

    created two constructors one that passes in a string message and calls super the other with a pre-made message. the classes extends the Exception class

--- JavaAdvanced/Advanced/src/com/jethro/exceptions/InsufficientFundsException.java
+++ JavaAdvanced/Advanced/src/com/jethro/exceptions/InsufficientFundsException.java
@@ -1,4 +1,13 @@
 package com.jethro.exceptions;
+// checked ->  Exception class
+// unchecked (runtime exception) -> Runtime Exception
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException() {
+        super("Insufficient funds in your account");
+    }
 
-public class InsufficientFundsException {
+    public InsufficientFundsException(String message) {
+        super(message
+        );
+    }
 }
