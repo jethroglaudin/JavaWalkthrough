8d815f901e47198bdbb2b0597fe9d5ab62e535c5
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-04 18:14:26 -0400

    modified Utils class created a Generic method

--- JavaAdvanced/Advanced/src/com/jethro/generics/Utils.java
+++ JavaAdvanced/Advanced/src/com/jethro/generics/Utils.java
@@ -1,4 +1,7 @@
 package com.jethro.generics;
 
 public class Utils {
+    public static <T extends Comparable <T>> T max(T first, T second) {
+        return (first.compareTo(second) < 0) ? second : first;
+    }
 }
