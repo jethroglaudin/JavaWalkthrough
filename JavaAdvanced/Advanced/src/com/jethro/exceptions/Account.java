058fc175861e3688f90fa3319d81f228d732a281
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-03 22:38:22 -0400

    created field name balance thats of data type float then created method withdraw that throws a new InsufficientFundsException should value be larger than balance

--- JavaAdvanced/Advanced/src/com/jethro/exceptions/Account.java
+++ JavaAdvanced/Advanced/src/com/jethro/exceptions/Account.java
@@ -3,9 +3,18 @@ package com.jethro.exceptions;
 import java.io.IOException;
 
 public class Account {
+    private float balance;
+
     public void deposit(float value) throws IOException {
         if (value <= 0 ) // defensive programming
 //            throw new IllegalArgumentException();
             throw new IOException();
+    }
+
+    // Creating Generic Exception
+    public void withdraw(float value) throws InsufficientFundsException {
+        if (value > balance)
+            throw new InsufficientFundsException();
+
     }
 }
