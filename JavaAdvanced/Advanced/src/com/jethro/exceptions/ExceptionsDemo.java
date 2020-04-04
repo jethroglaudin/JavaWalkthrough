3833254441e5033ffedf20d42278cf8b79260d41
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-03 22:55:48 -0400

    refactored the try catch to catch this time an AccountException where I used getCause to get the cause of the exception. Followed by printing to the terminal the cause using .getMessage() method to get the message

--- JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/exceptions/ExceptionsDemo.java
@@ -12,8 +12,9 @@ public class ExceptionsDemo {
         var account = new Account();
         try {
             account.withdraw(10);
-        } catch (InsufficientFundsException e) {
-            System.out.println(e.getMessage());
+        } catch (AccountException e) {
+            var cause = e.getCause();
+            System.out.println(cause.getMessage());
         }
         try {
             account.deposit(-1);
