9c4f488e8dcb19ec3438dbe238e485ebb54cb37c
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-04 18:26:17 -0400

    modified main class - Generic Methods

--- JavaAdvanced/Advanced/src/com/jethro/Main.java
+++ JavaAdvanced/Advanced/src/com/jethro/Main.java
@@ -4,20 +4,23 @@ import com.jethro.exceptions.ExceptionsDemo;
 import com.jethro.generics.GenericList;
 import com.jethro.generics.List;
 import com.jethro.generics.User;
+import com.jethro.generics.Utils;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        var max = Utils.max(new User(10), new User(20));
+        System.out.println(max);
 
-        var user1 = new User(10);
-        var user2 = new User(20);
-        if(user1.compareTo(user2) < 0)
-            System.out.println("user1 < user2");
-        else if (user1.compareTo(user2) == 0)
-            System.out.println("user1 == user2");
-        else
-            System.out.println("user1 > user2");
+//        var user1 = new User(10);
+//        var user2 = new User(20);
+//        if(user1.compareTo(user2) < 0)
+//            System.out.println("user1 < user2");
+//        else if (user1.compareTo(user2) == 0)
+//            System.out.println("user1 == user2");
+//        else
+//            System.out.println("user1 > user2");
 
 //        GenericList<Integer> numbers = new GenericList();
 //        numbers.add(1); // java compilier will wrap this interger to the Interger class (boxing)
