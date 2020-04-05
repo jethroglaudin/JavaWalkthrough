337d0be343e9ff3984d570c249e0060c1fc26f65
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-04 23:17:14 -0400

    added new method for printing Users passing GenericList<User> as a paramater. implementation not complete yet

--- JavaAdvanced/Advanced/src/com/jethro/generics/Utils.java
+++ JavaAdvanced/Advanced/src/com/jethro/generics/Utils.java
@@ -8,4 +8,12 @@ public class Utils {
     public static <K, V>void print(K key, V value) {
         System.out.println(key + "=" + value);
     }
+
+    public static void printUser(User user) {
+        System.out.println(user);
+    }
+
+    public static void printUsers(GenericList<User> users){
+
+    }
 }
