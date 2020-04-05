72720e07715b67b007499dc7270ff7205a7bf585
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-04 23:08:53 -0400

    created new method that holds multiple type paramaters

--- JavaAdvanced/Advanced/src/com/jethro/generics/Utils.java
+++ JavaAdvanced/Advanced/src/com/jethro/generics/Utils.java
@@ -4,4 +4,8 @@ public class Utils {
     public static <T extends Comparable <T>> T max(T first, T second) {
         return (first.compareTo(second) < 0) ? second : first;
     }
+
+    public static <K, V>void print(K key, V value) {
+        System.out.println(key + "=" + value);
+    }
 }
