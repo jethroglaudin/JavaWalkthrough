e31a8afcf0cfecd63adae494e60c150a6ea9eb92
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-05 22:15:35 -0400

    Iterator interface - completed iterator method

--- JavaAdvanced/Advanced/src/com/jethro/generics/GenericList.java
+++ JavaAdvanced/Advanced/src/com/jethro/generics/GenericList.java
@@ -17,6 +17,24 @@ public class GenericList <T> implements  Iterable<T>{
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new ListIterator(this);
+    }
+
+    private class ListIterator implements Iterator<T> {
+        private GenericList<T> list;
+        private int index;
+        public ListIterator(GenericList<T> list) {
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
     }
 }
