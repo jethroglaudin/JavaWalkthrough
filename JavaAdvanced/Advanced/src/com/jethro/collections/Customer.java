87a8e228a2abbcce5f7b3adab2e2fd4b11addf8b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-06 22:55:44 -0400

    refactored constructor to take email when instantiated. created getter and setter for email

--- JavaAdvanced/Advanced/src/com/jethro/collections/Customer.java
+++ JavaAdvanced/Advanced/src/com/jethro/collections/Customer.java
@@ -3,8 +3,11 @@ package com.jethro.collections;
 public class Customer implements Comparable<Customer> {
     private String name;
 
-    public Customer(String name) {
+    private String email;
+
+    public Customer(String name, String email) {
         this.name = name;
+        this.email = email;
     }
 
     @Override
@@ -15,5 +18,13 @@ public class Customer implements Comparable<Customer> {
     @Override
     public String toString() {
         return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
