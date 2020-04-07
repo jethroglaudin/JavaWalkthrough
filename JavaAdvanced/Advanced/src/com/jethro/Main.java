d2da393e3c41cee9b7a13e0c8f6b49eb5779235d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-06 22:09:32 -0400

    working with List interface which extends the collections interface

--- JavaAdvanced/Advanced/src/com/jethro/Main.java
+++ JavaAdvanced/Advanced/src/com/jethro/Main.java
@@ -1,6 +1,7 @@
 package com.jethro;
 
 import com.jethro.collections.CollectionDemo;
+import com.jethro.collections.ListDemo;
 import com.jethro.exceptions.ExceptionsDemo;
 import com.jethro.generics.*;
 
@@ -8,7 +9,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        CollectionDemo.show();
+//        CollectionDemo.show();
+        ListDemo.show();
 
 
 //        var list = new GenericList<String>();
