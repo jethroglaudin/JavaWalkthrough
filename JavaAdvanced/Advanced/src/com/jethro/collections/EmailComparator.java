74c262ad4ed3feab04b8c1cdd655b8b3f93e8864
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-06 22:54:17 -0400

    EmailComparator class implements Comparator interface to compare Customer object. Override compare method. used getter method to get email from Customer class

--- JavaAdvanced/Advanced/src/com/jethro/collections/EmailComparator.java
+++ JavaAdvanced/Advanced/src/com/jethro/collections/EmailComparator.java
@@ -1,4 +1,11 @@
 package com.jethro.collections;
 
-public class EmailComparator {
+import java.util.Comparator;
+
+public class EmailComparator  implements Comparator <Customer> {
+
+    @Override
+    public int compare(Customer o1, Customer o2) {
+        return o1.getEmail().compareTo(o2.getEmail());
+    }
 }
