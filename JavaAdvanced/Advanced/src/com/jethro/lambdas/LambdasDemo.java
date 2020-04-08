f40267975cbdf36193082fcf754888f3aa729b4e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-08 19:45:28 -0400

    Aonymous Inner classes

--- JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
@@ -2,7 +2,13 @@ package com.jethro.lambdas;
 
 public class LambdasDemo {
     public static void show(){
-        greet(new ConsolePrinter());
+//        greet(new ConsolePrinter());
+        greet(new Printer() { // Anonymous Inner class. Class does not have a name but an implementation. We can achieve the same result with less code.
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
     }
     public static void greet(Printer printer){
         printer.print("Hello World");
