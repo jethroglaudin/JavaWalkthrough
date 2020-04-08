1178fd3c016027abe73e1a934c99a04dba3bf6a7
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-07 22:40:27 -0400

    declared a variable Queue of type string that set to a new ArrayDeque. practice with methods like add, offer, and peek

--- JavaAdvanced/Advanced/src/com/jethro/collections/QueueDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/collections/QueueDemo.java
@@ -1,4 +1,21 @@
 package com.jethro.collections;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class QueueDemo {
+    public static void show(){
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("c");
+        queue.add("a");
+        queue.add("b");
+        queue.offer("d");
+        // d -> b -> a -> c
+        // in some cases where queue has limited size the add method throws exception and the offer method throws false. (less aggressive)
+
+        var front = queue.peek();
+        System.out.println(front);
+
+    }
+
 }
