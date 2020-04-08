baa7c5b67ccb51fe826ee72a4060faf9af9fb989
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-08 19:37:00 -0400

    created two static methods one that takes the Printer interface as a paramater the other that calls the other method (greet) and passes new instance of the ConsolePrinter class

--- JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
@@ -1,4 +1,10 @@
 package com.jethro.lambdas;
 
 public class LambdasDemo {
+    public static void show(){
+        greet(new ConsolePrinter());
+    }
+    public static void greet(Printer printer){
+        printer.print("Hello World");
+    }
 }
