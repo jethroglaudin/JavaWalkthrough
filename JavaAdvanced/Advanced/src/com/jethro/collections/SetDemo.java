549db9b9332a4c6b743c8d4a5bf50610984ae47d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-07 23:10:52 -0400

    The set interface - working with different methods form the Set interface. Key point sets do not guarantee the order of the item. It guarantees uniqueness.

--- JavaAdvanced/Advanced/src/com/jethro/collections/SetDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/collections/SetDemo.java
@@ -1,4 +1,37 @@
 package com.jethro.collections;
 
+import java.util.*;
+
 public class SetDemo {
+    public static void show() {
+        Set<String> set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
+        Set<String> set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
+
+        // Union which the combination of two sets
+//        set1.addAll(set2);
+//        System.out.println(set1);
+
+        // Intersection which will give us the items that are common in both sets
+//        set1.retainAll(set2);
+//        System.out.println(set1);
+
+        // Difference
+        set1.removeAll(set2);
+        System.out.println(set1);
+
+
+
+//        Collection<String> collection = new ArrayList<>();
+//        Collections.addAll(collection, "a", "b", "c", "c");
+//        Set<String> set = new HashSet<>(collection); // removes duplicate
+//        System.out.println(set);
+
+//        Set<String> set = new HashSet<>();
+//        set.add("sky");
+//        set.add("is");
+//        set.add("blue");
+//        set.add("blue");
+        // Set interface does not guarantee the order of the items. Set only guarantees uniqueness.
+//        System.out.println(set);
+    }
 }
