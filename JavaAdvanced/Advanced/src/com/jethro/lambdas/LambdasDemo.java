fa16da6f8b2cbbcffcf279f0684d1f25af2b3626
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-08 19:54:07 -0400

    Practice with Lambda expressions with methods and variables.

--- JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
@@ -3,12 +3,18 @@ package com.jethro.lambdas;
 public class LambdasDemo {
     public static void show(){
 //        greet(new ConsolePrinter());
-        greet(new Printer() { // Anonymous Inner class. Class does not have a name but an implementation. We can achieve the same result with less code.
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
-        });
+        // Lambda expressions are essentially objects, but we can use them to represent anonymous functions
+        greet(message -> System.out.println(message)); // Lambda Expression
+        Printer printer = message -> System.out.println(message); // Lambda expression as a variable
+
+
+
+//        greet(new Printer() { // Anonymous Inner class. Class does not have a name but an implementation. We can achieve the same result with less code.
+//            @Override
+//            public void print(String message) {
+//                System.out.println(message);
+//            }
+//        });
     }
     public static void greet(Printer printer){
         printer.print("Hello World");
