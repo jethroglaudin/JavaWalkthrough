772cbb5a33cad3fa24e6d9b4685a14b1ad0dae32
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-07 22:46:03 -0400

    practice with using methods like remove, poll, peek and element from the queue interface)

--- JavaAdvanced/Advanced/src/com/jethro/collections/QueueDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/collections/QueueDemo.java
@@ -11,10 +11,15 @@ public class QueueDemo {
         queue.add("b");
         queue.offer("d");
         // d -> b -> a -> c
-        // in some cases where queue has limited size the add method throws exception and the offer method throws false. (less aggressive)
+        // in some cases where queue has limited size, the add method throws exception and the offer method throws false. (less aggressive)
 
-        var front = queue.peek();
+//        var front = queue.peek();
+        var front = queue.remove(); // removes item at front of the queue and return it
+//        var front = queue.poll(); // similar to remove but poll() returns null if queue is empty while remove() throws an exception
+//        var front = queue.element();
+        // peek returns null if the queue is empty. element() throws an exception.
         System.out.println(front);
+        System.out.println(queue);
 
     }
 
