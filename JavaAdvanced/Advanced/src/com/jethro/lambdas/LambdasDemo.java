bf7a8143c0baeb82f77cecd804ce6b451901857e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-09 23:11:30 -0400

    Method references

--- JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
@@ -1,11 +1,19 @@
 package com.jethro.lambdas;
 
 public class LambdasDemo {
-    public String prefix = "-";
-    public void show(){
+    public LambdasDemo(String message) {
+    }
+    public static void print (String message){}
+
+    public static void show(){
+        greet(LambdasDemo::print);
+        greet(message -> new LambdasDemo());
+        greet(LambdasDemo::new); // Method references through constructor.
+        //Class/Object::method
+
 //        greet(new ConsolePrinter());
         // Lambda expressions are essentially objects, but we can use them to represent anonymous functions
-        greet(message -> System.out.println(prefix + message)); // Lambda Expression
+//        greet(message -> System.out.println(message)); // Lambda Expression
 //        Printer printer = message -> System.out.println(message); // Lambda expression as a variable
 
 //        greet(new Printer() { // Anonymous Inner class. Class does not have a name but an implementation. We can achieve the same result with less code.
