00c2aa7dda5f5026996751780e55da29f712b59a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-09 22:57:11 -0400

    Variable capture

--- JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
@@ -1,11 +1,12 @@
 package com.jethro.lambdas;
 
 public class LambdasDemo {
+    public static String prefix = "-";
     public static void show(){
 //        greet(new ConsolePrinter());
         // Lambda expressions are essentially objects, but we can use them to represent anonymous functions
-        greet(message -> System.out.println(message)); // Lambda Expression
-        Printer printer = message -> System.out.println(message); // Lambda expression as a variable
+        greet(message -> System.out.println(prefix + message)); // Lambda Expression
+//        Printer printer = message -> System.out.println(message); // Lambda expression as a variable
 
 //        greet(new Printer() { // Anonymous Inner class. Class does not have a name but an implementation. We can achieve the same result with less code.
 //            @Override
