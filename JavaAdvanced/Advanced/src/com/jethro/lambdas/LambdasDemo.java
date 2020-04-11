07a3aeacfc2a10acde2bb6487ba3dac2259dcb48
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-11 18:42:48 -0400

    The Predicate Interface - useful for filter through data. Has a single abstract method called test. Returns a boolean based on how the object that is passed meets the criteria we set

--- JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/lambdas/LambdasDemo.java
@@ -3,10 +3,15 @@ package com.jethro.lambdas;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class LambdasDemo {
     public static void show() {
+        Predicate<String> isLongerThan5 = str -> str.length() > 5;
+        var predicateResult = isLongerThan5.test("sky");
+        System.out.println(predicateResult);
+
         Function<String, String> replaceColon = str -> str.replace(":", "=");
         Function<String, String> addBraces = str -> "{" + str + "}";
         var result = replaceColon
