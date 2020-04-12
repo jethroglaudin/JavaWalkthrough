2b40c8c6b415c7096be38650bd63df679958100f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-12 16:35:02 -0400

    Imperative vs Functional (Declarative) programming using stream for an example

--- JavaAdvanced/Advanced/src/com/jethro/streams/StreamsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/streams/StreamsDemo.java
@@ -1,4 +1,28 @@
 package com.jethro.streams;
 
+import java.util.List;
+
 public class StreamsDemo {
+    public static void show(){
+        List<Movie> movies = List.of(
+                new Movie("A Quiet Place", 10),
+                new Movie("Casablanca", 15),
+                new Movie("The GodFather", 20)
+        );
+
+        int count = 0;
+        // This is an example of imperative programming. Statements that specify how somthing should be done
+        // These statements determine how many movies have likes greater than 10.
+        for (var movie : movies) {
+            if (movie.getLikes() > 10)
+                count++;
+        }
+        // Stream is a way to get data out of a collection.
+        // Declarative (Functional) Programming
+       var count2 =  movies.stream()
+                .filter(movie -> movie.getLikes() > 10)
+                .count();
+
+    }
+
 }
