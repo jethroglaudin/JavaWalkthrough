686e9eb9733925206494d11095fa4d41ebb6d191
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-12 16:20:29 -0400

    created two private fields for storing title and likes. Also created a constructor and passed  two arguments (title and likes).Finally created a getter for likes to retrieve likes

--- JavaAdvanced/Advanced/src/com/jethro/streams/Movie.java
+++ JavaAdvanced/Advanced/src/com/jethro/streams/Movie.java
@@ -1,4 +1,15 @@
 package com.jethro.streams;
 
 public class Movie {
+    private String title;
+    private  int likes;
+
+    public Movie(String title, int likes) {
+        this.title = title;
+        this.likes = likes;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
 }
