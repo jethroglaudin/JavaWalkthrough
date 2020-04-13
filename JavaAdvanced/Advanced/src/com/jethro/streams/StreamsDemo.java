42c16ada14a265e2c955d1d550b5f2ef1d274cc0
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-12 20:30:54 -0400

    Slicing Streams - practice with 4 seperate methods for slicing streams. limit, skip, takeWhile and dropWhile

--- JavaAdvanced/Advanced/src/com/jethro/streams/StreamsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/streams/StreamsDemo.java
@@ -8,13 +8,22 @@ public class StreamsDemo {
     public static void show(){
         List<Movie> movies = List.of(
                 new Movie("A Quiet Place", 10),
-                new Movie("Casablanca", 15),
+                new Movie("Casablanca", 30),
                 new Movie("The GodFather", 20)
         );
-        Predicate<Movie> isPopular = movie -> movie.getLikes() > 10;
+
         movies.stream()
-                .filter(isPopular) // returns a new stream of movies
+//                .limit(2)
+//                .skip(2)
+//                .takeWhile(movie -> movie.getLikes() < 30)
+                .dropWhile(movie -> movie.getLikes() < 30)
                 .forEach(movie -> System.out.println(movie.getTitle()));
+        // this will stop the moment it get to 30. Filter iterates the entire data source. takeWhile will stop the moment the predicate is false
+        // opposite of takeWhile escapes all the elements that matches the predicate
+//        Predicate<Movie> isPopular = movie -> movie.getLikes() > 10;
+//        movies.stream()
+//                .filter(isPopular) // returns a new stream of movies
+//                .forEach(movie -> System.out.println(movie.getTitle()));
 //        movies.stream()
 ////                .mapToInt(movie -> movie.getLikes())
 //                .map(movie -> movie.getTitle())
