cc379f689e61999dcc38b52048055014e906e837
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-04-12 20:12:38 -0400

    Filtering Elements - practice using filter method with stream. Also stored a Predicate of Movie called isPopular and passing it within filter since a the filter method takes a Predicate and returns a new stream based on the boolean it returns.

--- JavaAdvanced/Advanced/src/com/jethro/streams/StreamsDemo.java
+++ JavaAdvanced/Advanced/src/com/jethro/streams/StreamsDemo.java
@@ -1,25 +1,29 @@
 package com.jethro.streams;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void show(){
-        var stream = Stream.of(List.of(1,2,3), List.of(4, 5, 6));
-        stream
-                .flatMap(list -> list.stream()) // convert each list into a stream and flatten it.
-                .forEach(num -> System.out.println(num));
-
         List<Movie> movies = List.of(
                 new Movie("A Quiet Place", 10),
                 new Movie("Casablanca", 15),
                 new Movie("The GodFather", 20)
         );
+        Predicate<Movie> isPopular = movie -> movie.getLikes() > 10;
         movies.stream()
-//                .mapToInt(movie -> movie.getLikes())
-                .map(movie -> movie.getTitle())
-                .forEach(name -> System.out.println(name));
+                .filter(isPopular) // returns a new stream of movies
+                .forEach(movie -> System.out.println(movie.getTitle()));
+//        movies.stream()
+////                .mapToInt(movie -> movie.getLikes())
+//                .map(movie -> movie.getTitle())
+//                .forEach(name -> System.out.println(name));
 
+//        var stream = Stream.of(List.of(1,2,3), List.of(4, 5, 6));
+//        stream
+//                .flatMap(list -> list.stream()) // convert each list into a stream and flatten it.
+//                .forEach(num -> System.out.println(num));
 //        int count = 0;
 //        // This is an example of imperative programming. Statements that specify how somthing should be done
 //        // These statements determine how many movies have likes greater than 10.
